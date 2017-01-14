35269d4cf9edd9e94467922070fef6326bb88674
Titouan Vervack <titouan.vervack@sigasi.com>
2017-01-14 15:05:58 +0000

    Fixed spaces after ML comments
    Signed-off-by: Titouan Vervack <titouan.vervack@sigasi.com>

--- org.eclipse.xtext.tests/xtend-gen/org/eclipse/xtext/formatting2/internal/CommentWithSpacesFormatterTest.java
+++ org.eclipse.xtext.tests/xtend-gen/org/eclipse/xtext/formatting2/internal/CommentWithSpacesFormatterTest.java
@@ -1,23 +1,29 @@
 /**
  * Copyright (c) 2017 TypeFox GmbH (http://www.typefox.io) and others.
- * This program and the accompanying materials are made available under the
- * terms of the Eclipse Public License 2.0 which is available at
- * http://www.eclipse.org/legal/epl-2.0.
- * 
- * SPDX-License-Identifier: EPL-2.0
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
  */
 package org.eclipse.xtext.formatting2.internal;
 
+import com.google.common.base.Objects;
 import com.google.inject.Inject;
 import org.eclipse.xtend2.lib.StringConcatenation;
+import org.eclipse.xtext.formatting2.IAutowrapFormatter;
 import org.eclipse.xtext.formatting2.IFormattableDocument;
 import org.eclipse.xtext.formatting2.IHiddenRegionFormatter;
+import org.eclipse.xtext.formatting2.IHiddenRegionFormatting;
+import org.eclipse.xtext.formatting2.ITextReplacer;
+import org.eclipse.xtext.formatting2.ITextReplacerContext;
 import org.eclipse.xtext.formatting2.internal.GenericFormatter;
 import org.eclipse.xtext.formatting2.internal.GenericFormatterTestRequest;
 import org.eclipse.xtext.formatting2.internal.GenericFormatterTester;
+import org.eclipse.xtext.formatting2.internal.WhitespaceReplacer;
 import org.eclipse.xtext.formatting2.internal.formattertestlanguage.IDList;
 import org.eclipse.xtext.formatting2.internal.tests.FormatterTestLanguageInjectorProvider;
 import org.eclipse.xtext.formatting2.regionaccess.ITextRegionExtensions;
+import org.eclipse.xtext.formatting2.regionaccess.ITextSegment;
 import org.eclipse.xtext.testing.InjectWith;
 import org.eclipse.xtext.testing.XtextRunner;
 import org.eclipse.xtext.xbase.lib.Extension;
@@ -31,14 +37,77 @@ import org.junit.runner.RunWith;
 @RunWith(XtextRunner.class)
 @InjectWith(FormatterTestLanguageInjectorProvider.class)
 @SuppressWarnings("all")
-public class CommentFormatterTest {
-  public static class CustomFormatter extends GenericFormatter<IDList> {
+public class CommentWithSpacesFormatterTest {
+  public static class TabsAndSpacesSupportingFormatter extends GenericFormatter<IDList> {
     @Override
     public void format(final IDList model, final ITextRegionExtensions regionAccess, @Extension final IFormattableDocument document) {
       final Procedure1<IHiddenRegionFormatter> _function = (IHiddenRegionFormatter it) -> {
         it.oneSpace();
       };
-      document.append(this.textRegionExtensions.regionFor(model).keyword("idlist"), _function);
+      final Procedure1<IHiddenRegionFormatter> _function_1 = (IHiddenRegionFormatter it) -> {
+        it.setNewLines(0);
+        it.highPriority();
+      };
+      document.append(document.append(this.textRegionExtensions.regionFor(model).keyword("idlist"), _function), _function_1);
+    }
+    
+    @Override
+    public ITextReplacer createWhitespaceReplacer(final ITextSegment hiddens, final IHiddenRegionFormatting formatting) {
+      return new CommentWithSpacesFormatterTest.TabAndSpacesSupportingWhiteSpaceReplacer(hiddens, formatting);
+    }
+  }
+  
+  private static class TabAndSpacesSupportingWhiteSpaceReplacer extends WhitespaceReplacer {
+    public TabAndSpacesSupportingWhiteSpaceReplacer(final ITextSegment whitespace, final IHiddenRegionFormatting formatting) {
+      super(whitespace, formatting);
+    }
+    
+    /**
+     * Copied from {@link WhitespaceReplacer}
+     */
+    @Override
+    public ITextReplacerContext createReplacements(final ITextReplacerContext context) {
+      if (((this.getFormatting().getAutowrap() != null) && ((this.getFormatting().getAutowrap()).intValue() >= 0))) {
+        context.setCanAutowrap(this.getFormatting().getAutowrap());
+      }
+      final String space = this.getFormatting().getSpace();
+      final int trailingNewLinesOfPreviousRegion = this.trailingNewLinesOfPreviousRegion();
+      final int computedNewLineCount = this.computeNewLineCount(context);
+      int newLineCount = Math.max((computedNewLineCount - trailingNewLinesOfPreviousRegion), 0);
+      if (((newLineCount == 0) && context.isAutowrap())) {
+        final IAutowrapFormatter onAutowrap = this.getFormatting().getOnAutowrap();
+        if ((onAutowrap != null)) {
+          onAutowrap.format(this.getRegion(), this.getFormatting(), context.getDocument());
+        }
+        newLineCount = 1;
+      }
+      final int indentationCount = this.computeNewIndentation(context);
+      if (((newLineCount == 0) && (trailingNewLinesOfPreviousRegion == 0))) {
+        if ((space != null)) {
+          context.addReplacement(this.getRegion().replaceWith(space));
+        }
+      } else {
+        Boolean _noIndentation = this.getFormatting().getNoIndentation();
+        final boolean noIndentation = Objects.equal(_noIndentation, Boolean.TRUE);
+        final String newLines = context.getNewLinesString(newLineCount);
+        String _xifexpression = null;
+        if (noIndentation) {
+          _xifexpression = "";
+        } else {
+          _xifexpression = context.getIndentationString(indentationCount);
+        }
+        final String indentation = _xifexpression;
+        ITextSegment _region = this.getRegion();
+        String _elvis = null;
+        if (space != null) {
+          _elvis = space;
+        } else {
+          _elvis = "";
+        }
+        String _plus = ((newLines + indentation) + _elvis);
+        context.addReplacement(_region.replaceWith(_plus));
+      }
+      return context.withIndentation(indentationCount);
     }
   }
   
@@ -55,11 +124,12 @@ public class CommentFormatterTest {
       _builder.append("a");
       _builder.newLine();
       it.setToBeFormatted(_builder);
-      CommentFormatterTest.CustomFormatter _customFormatter = new CommentFormatterTest.CustomFormatter();
-      it.setFormatter(_customFormatter);
+      CommentWithSpacesFormatterTest.TabsAndSpacesSupportingFormatter _tabsAndSpacesSupportingFormatter = new CommentWithSpacesFormatterTest.TabsAndSpacesSupportingFormatter();
+      it.setFormatter(_tabsAndSpacesSupportingFormatter);
       StringConcatenation _builder_1 = new StringConcatenation();
       _builder_1.append("idlist //x");
       _builder_1.newLine();
+      _builder_1.append(" ");
       _builder_1.append("a");
       _builder_1.newLine();
       it.setExpectation(_builder_1);
@@ -81,8 +151,8 @@ public class CommentFormatterTest {
       _builder.append("a");
       _builder.newLine();
       it.setToBeFormatted(_builder);
-      CommentFormatterTest.CustomFormatter _customFormatter = new CommentFormatterTest.CustomFormatter();
-      it.setFormatter(_customFormatter);
+      CommentWithSpacesFormatterTest.TabsAndSpacesSupportingFormatter _tabsAndSpacesSupportingFormatter = new CommentWithSpacesFormatterTest.TabsAndSpacesSupportingFormatter();
+      it.setFormatter(_tabsAndSpacesSupportingFormatter);
       StringConcatenation _builder_1 = new StringConcatenation();
       _builder_1.append("idlist //x");
       _builder_1.newLine();
@@ -100,27 +170,10 @@ public class CommentFormatterTest {
       _builder.append("idlist  /*x*/  a");
       _builder.newLine();
       it.setToBeFormatted(_builder);
-      CommentFormatterTest.CustomFormatter _customFormatter = new CommentFormatterTest.CustomFormatter();
-      it.setFormatter(_customFormatter);
+      CommentWithSpacesFormatterTest.TabsAndSpacesSupportingFormatter _tabsAndSpacesSupportingFormatter = new CommentWithSpacesFormatterTest.TabsAndSpacesSupportingFormatter();
+      it.setFormatter(_tabsAndSpacesSupportingFormatter);
       StringConcatenation _builder_1 = new StringConcatenation();
       _builder_1.append("idlist /*x*/ a");
-      _builder_1.newLine();
-      it.setExpectation(_builder_1);
-    };
-    this._genericFormatterTester.assertFormatted(_function);
-  }
-  
-  @Test
-  public void MLSL_double_inline() {
-    final Procedure1<GenericFormatterTestRequest> _function = (GenericFormatterTestRequest it) -> {
-      StringConcatenation _builder = new StringConcatenation();
-      _builder.append("idlist  /*x*//*y*/  a");
-      _builder.newLine();
-      it.setToBeFormatted(_builder);
-      CommentFormatterTest.CustomFormatter _customFormatter = new CommentFormatterTest.CustomFormatter();
-      it.setFormatter(_customFormatter);
-      StringConcatenation _builder_1 = new StringConcatenation();
-      _builder_1.append("idlist /*x*/ /*y*/ a");
       _builder_1.newLine();
       it.setExpectation(_builder_1);
     };
@@ -142,8 +195,8 @@ public class CommentFormatterTest {
       _builder.append("a");
       _builder.newLine();
       it.setToBeFormatted(_builder);
-      CommentFormatterTest.CustomFormatter _customFormatter = new CommentFormatterTest.CustomFormatter();
-      it.setFormatter(_customFormatter);
+      CommentWithSpacesFormatterTest.TabsAndSpacesSupportingFormatter _tabsAndSpacesSupportingFormatter = new CommentWithSpacesFormatterTest.TabsAndSpacesSupportingFormatter();
+      it.setFormatter(_tabsAndSpacesSupportingFormatter);
       StringConcatenation _builder_1 = new StringConcatenation();
       _builder_1.append("idlist /*x*/");
       _builder_1.newLine();
@@ -165,8 +218,8 @@ public class CommentFormatterTest {
       _builder.append("*/  a");
       _builder.newLine();
       it.setToBeFormatted(_builder);
-      CommentFormatterTest.CustomFormatter _customFormatter = new CommentFormatterTest.CustomFormatter();
-      it.setFormatter(_customFormatter);
+      CommentWithSpacesFormatterTest.TabsAndSpacesSupportingFormatter _tabsAndSpacesSupportingFormatter = new CommentWithSpacesFormatterTest.TabsAndSpacesSupportingFormatter();
+      it.setFormatter(_tabsAndSpacesSupportingFormatter);
       StringConcatenation _builder_1 = new StringConcatenation();
       _builder_1.append("idlist");
       _builder_1.newLine();
@@ -176,9 +229,7 @@ public class CommentFormatterTest {
       _builder_1.append("* x");
       _builder_1.newLine();
       _builder_1.append(" ");
-      _builder_1.append("*/");
-      _builder_1.newLine();
-      _builder_1.append("a");
+      _builder_1.append("*/ a");
       _builder_1.newLine();
       it.setExpectation(_builder_1);
     };
@@ -203,15 +254,13 @@ public class CommentFormatterTest {
       _builder.newLine();
       _builder.append("a");
       _builder.newLine();
+      _builder.append("b");
+      _builder.newLine();
       it.setToBeFormatted(_builder);
-      CommentFormatterTest.CustomFormatter _customFormatter = new CommentFormatterTest.CustomFormatter();
-      it.setFormatter(_customFormatter);
+      CommentWithSpacesFormatterTest.TabsAndSpacesSupportingFormatter _tabsAndSpacesSupportingFormatter = new CommentWithSpacesFormatterTest.TabsAndSpacesSupportingFormatter();
+      it.setFormatter(_tabsAndSpacesSupportingFormatter);
       StringConcatenation _builder_1 = new StringConcatenation();
-      _builder_1.append("idlist");
-      _builder_1.newLine();
-      _builder_1.newLine();
-      _builder_1.newLine();
-      _builder_1.append("/*");
+      _builder_1.append("idlist /*");
       _builder_1.newLine();
       _builder_1.append(" ");
       _builder_1.append("* x");
@@ -220,6 +269,8 @@ public class CommentFormatterTest {
       _builder_1.append("*/");
       _builder_1.newLine();
       _builder_1.append("a");
+      _builder_1.newLine();
+      _builder_1.append("b");
       _builder_1.newLine();
       it.setExpectation(_builder_1);
     };
